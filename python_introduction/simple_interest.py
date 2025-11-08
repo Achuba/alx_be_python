a6c4ff0cd66ece850fa57150e3529b50e9da4ded
Achuba <achubadaniel45@gmail.com>
2025-11-08 18:50:02 +0100

    new changes

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3 
 
-Interest = (principal) * (rate) * (time)
+interest = principal * rate * time
 
-print(f"The simple Interest is: {Interest}") # Output: The simple Interest is: 150.0
+print(f"The simple Interest is: {interest}") # Output: The simple Interest is: 150.0
