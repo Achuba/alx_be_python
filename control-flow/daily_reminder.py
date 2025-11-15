b4ac7a24607d694d2ea02c9b5f09ace860ac6368
Achuba <achubadaniel45@gmail.com>
2025-11-15 17:54:49 +0100

    bug fixes

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -12,24 +12,26 @@ while run:
                     reminder += " that requires immediate attention today!"
             else:
                 reminder += ". Consider completing it as soon as possible."
+            print(reminder)
         case "medium":
             reminder = f"Reminder: '{task}' is a medium priority task"
             if time_bound == "yes":
                 reminder += " that requires attention today!"
             else:
                 reminder += ". Try to complete it soon."
+            print(reminder)
         case "low":
             reminder = f"Note: '{task}' is a low priority task"
             if time_bound == "yes":
                 reminder += " that you might want to address today."
             else:
-                reminder += ". You can do it whenever you have free time."  
+                reminder += ". You can do it whenever you have free time."
+            print(reminder)
         case _:
             reminder = "Unrecognized priority level."
             print(reminder)
             break 
- 
-    print(reminder)
+
 
   
     run = False
