f98f06b76db51bf1a15ed38fa7226fe000e2c7c3
Achuba <achubadaniel45@gmail.com>
2025-11-28 17:58:23 +0100

    Fixed more text errors

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,15 +10,15 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(5, 3), 8)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 4), 6)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
         self.assertEqual(self.calc.subtract(0, 5), -5)    
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(7, 6), 42)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(0, 100), 0)    
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(20, 4), 5)
         self.assertEqual(self.calc.divide(-9, 3), -3)
         self.assertIsNone(self.calc.divide(5, 0))  # Division by zero case
